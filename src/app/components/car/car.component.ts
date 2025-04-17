a6cd102f3d100f02569012b33f308fb198dcbaa1
Esieth <Glynndylan36@gmail.com>
2025-04-17 13:04:24 +0100

    commit

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarApiService } from '../../services/car-api.service';
+import { ICar } from '../../interfaces/car';
 
 @Component({
   selector: 'app-car',
-  imports: [],
-  templateUrl: './car.component.html',
-  styleUrl: './car.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="card mb-3">
+      <img [src]="carData.imageURL" class="card-img-top" [style.width.px]="carImageWidth" alt="{{carData.make}} {{carData.model}}">
+      <div class="card-body">
+        <h5 class="card-title">{{carData.make}} {{carData.model}}</h5>
+        <p class="card-text">Year: {{carData.year}}</p>
+      </div>
+    </div>
+  `
 })
 export class CarComponent {
+  // With this – we receive in car information to this component so we can display!
+  @Input() carData!: ICar;
+
+  carImageWidth: number = 300; // This keeps our car image size pretty neat…
+
+  constructor(private _carAPIService: CarApiService) {}
 
 }
--- src/app/components/carlist/carlist.component.ts
+++ src/app/components/carlist/carlist.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarApiService } from '../../services/car-api.service';
+import { ICar, NewCar } from '../../interfaces/car';
+import { CarComponent } from '../car/car.component';
 
 @Component({
   selector: 'app-carlist',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CarComponent],
   templateUrl: './carlist.component.html',
-  styleUrl: './carlist.component.css'
+  styleUrls: ['./carlist.component.css']
 })
-export class CarlistComponent {
+export class CarlistComponent implements OnInit {
+  carsData: ICar[] = [];
+  show: boolean = false;
 
+  constructor(private _carAPIService: CarApiService) {}
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this._carAPIService.getCars().subscribe({
+      next: (data) => {
+        this.carsData = data;
+        this.show = true;
+      },
+      error: (err) => {
+        console.error('Error fetching cars:', err);
+        this.show = false;
+      }
+    });
+  }
+
+  addCar(make: string, model: string, year: string, imageURL: string): boolean {
+    let addCar: ICar = new NewCar(make, model, year, imageURL);
+    this._carAPIService.addCar(addCar).subscribe({
+      next: () => {
+        this.getCars(); // Refresh the list after adding
+      },
+      error: (err) => {
+        console.error('Error adding car:', err);
+      }
+    });
+    return false;
+  }
+
+  deleteCar(id: string): void {
+    this._carAPIService.deleteCar(id).subscribe({
+      next: () => {
+        this.getCars();
+      },
+      error: (err) => {
+        console.error('Error deleting car:', err);
+      }
+    });
+  }
 }
